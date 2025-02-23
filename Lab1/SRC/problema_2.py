8c302e100103a02d5155bd11e64de1d0883bab41
Magos-Andoni <andonixu@gmail.com>
2025-02-23 17:11:17 -0600

    Update problema_2.py

--- Lab1/SRC/problema_2.py
+++ Lab1/SRC/problema_2.py
@@ -1,1 +1,4 @@
-
+horas= int (input("Aight man, tell me how many hours did you punch in today? "))
+money= int (input ("Bet man, now tell me how much gold ya making out per hour... "))
+salario = horas * money
+print ("Those suits owe ya ",salario ,"dollars man")
