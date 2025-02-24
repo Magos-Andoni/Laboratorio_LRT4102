78c6d98ab77ce6873e255ceaa25d65a748933bd0
Magos-Andoni <andonixu@gmail.com>
2025-02-23 19:08:05 -0600

    Update problema_1.py

--- Lab1/SRC/problema_1.py
+++ Lab1/SRC/problema_1.py
@@ -1,4 +1,4 @@
-n= int (input("Whats the value of n?"))
-print (n)
-suma = sum(range(1, n + 1))
+n= int (input("What's the value of n?")) #asks for the limit n
+print (n) #prints n
+suma = sum(range(1, n + 1)) #sums n values from the range of 1 to n+1
 print (suma)
